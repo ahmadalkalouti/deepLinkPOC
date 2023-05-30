3bacda6513e10f09a49cdd2837773075e2c21236
ahmadalkalouti <46530715+ahmadalkalouti@users.noreply.github.com>
2023-05-30 11:03:07 +0300

    Initial Commit

---
+++ Deeplink POC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Deeplink POC
+//
+//  Created by Ahmad Al Kalouti on 30/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
